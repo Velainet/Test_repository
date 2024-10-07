916f3393475231f4dd3e65d23b3123891b9def12
Velainet <egorchekov2008@gmail.com>
2024-10-07 12:33:44 +0300

    Поставлена провека

--- IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
+++ IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
@@ -4,19 +4,17 @@ using namespace std;
     
 int main()
 {   
-    for (int i ; i==0 ; ){
+    for (int i=0 ; i==0 ; ){
     int u, s;
     cout << ("write the number: "), cin >> (s);
     cout << ("write another number , but bigger: "), cin >> (u);
-        if (s == u)
-        {
+        if (s != u){
             i = 1;
-        }
         while ((s-1) < u){
             cout << ("numbers....cool = ") << s << endl;
-            ++s;
-            
-           
+            ++s;           
+        }
+        s = 0;
         }
     }
 }
