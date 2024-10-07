576e1d6ef04fd16a7c5b1791d42af15e3343f296
Velainet <egorchekov2008@gmail.com>
2024-10-07 11:50:47 +0300

    Добавил проверку равных чисел

--- IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
+++ IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
@@ -4,12 +4,19 @@ using namespace std;
     
 int main()
 {   
+    for (int i;i==0;){
     int u, s;
     cout << ("write the number: "), cin >> (s);
     cout << ("write another number , but bigger: "), cin >> (u);
-    cout << ("numbers....cool = ") << s << endl;
-        while (s < u){
-            s = (s+1);
-            cout << ("numbers....cool = ") << s<< "\n" ;
+        if (s == u)
+        {
+            i = 1;
         }
+        while (s < u){
+            cout << ("numbers....cool = ") << s<< "\n" ;
+            s++;
+            cout << ("numbers....cool = ") << s << endl;
+           
+        }
+    }
 }
