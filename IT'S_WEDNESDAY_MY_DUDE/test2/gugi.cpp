e1ba5bf0d1a20c233c2f44b2a9e49f758db14dd7
Velainet <egorchekov2008@gmail.com>
2024-10-07 12:15:55 +0300

    Исправил  ошибку в цикле while

--- IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
+++ IT'S_WEDNESDAY_MY_DUDE/test2/gugi.cpp
@@ -4,7 +4,7 @@ using namespace std;
     
 int main()
 {   
-    for (int i;i==0;){
+    for (int i ; i==0 ; ){
     int u, s;
     cout << ("write the number: "), cin >> (s);
     cout << ("write another number , but bigger: "), cin >> (u);
@@ -12,10 +12,10 @@ int main()
         {
             i = 1;
         }
-        while (s < u){
-            cout << ("numbers....cool = ") << s<< "\n" ;
-            s++;
+        while ((s-1) < u){
             cout << ("numbers....cool = ") << s << endl;
+            ++s;
+            
            
         }
     }
