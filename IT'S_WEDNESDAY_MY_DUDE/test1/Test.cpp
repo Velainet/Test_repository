2378e2245fc716260b87939731c0a2ea55fe37d0
Velainet <egorchekov2008@gmail.com>
2024-10-08 13:51:45 +0300

    added new scripts

--- IT'S_WEDNESDAY_MY_DUDE/test1/Test.cpp
+++ IT'S_WEDNESDAY_MY_DUDE/test1/Test.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-using namespace std;
-int huch;
-int main() {
-    cout <<-9;
+using namespace std; 
+int main()
+{
+    int (max);
+    cin >> (max);
+    int numbers[max];      
+  
+    int i;       
+    cout << "Enter numbers" << endl;
+    while(i < max)
+    {
+        cin >> numbers[i];   
+        i++;
+    }
    
+    for (i =0; i < max; ++i)
+    {
+        cout << numbers[i] << "\t";
+    }
+    cout << endl;
 }
-
